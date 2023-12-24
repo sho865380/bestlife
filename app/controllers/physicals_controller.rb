692afede05a8d10a4960f19786683df72f12b175
shokawazoe <sho865380@gmail.com>
2023-12-25 02:50:35 +0900

    アイコンリンク先変更

--- app/controllers/physicals_controller.rb
+++ app/controllers/physicals_controller.rb
@@ -1,13 +1,16 @@
 class PhysicalsController < ApplicationController
   
   
-  
   def create
     @physical = Physical.new(physical_params)
+  
+    # 手動でユーザーを設定
+    @user = User.find(session[:user_id]) # または別の方法でユーザーを取得
+  
     if @physical.save
-      redirect_to some_path, notice: 'データが正常に保存されました。'
+      redirect_to user_path(@user), notice: 'データが正常に保存されました。'
     else
-      render "users/show"
+      render "users/form"
     end
   end
   
